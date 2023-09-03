a43b6e760e3c20d013c9f365e7e5cdb76acea989
shyam kachhadiya <shyam1ss15@gmail.com>
2023-09-03 18:26:34 +0000

    added correct widget allignment for expanded

--- lib/screens/product_detail_page/product_detail_page.dart
+++ lib/screens/product_detail_page/product_detail_page.dart
@@ -29,111 +29,118 @@ class ProductDetailPage extends StatelessWidget {
         builder: (context, state) {
           if (state is ProductDetailLoadedState) {
             return Stack(
-              children: [
-                ListView(
-                  children: [
-                    // Image Carousel covering 60% of the screen
-                    state.product.images != null &&
-                            state.product.images!.isNotEmpty
-                        ? Container(
-                            height: MediaQuery.of(context).size.height * 0.6,
-                            child: CarouselSlider(
-                              options: CarouselOptions(
-                                aspectRatio: 16 / 9,
-                                autoPlay: true,
-                                enlargeCenterPage: true,
-                                autoPlayInterval: const Duration(seconds: 3),
-                              ),
-                              items: state.product.images!.map((image) {
-                                return Image.network(image);
-                              }).toList(),
-                            ),
-                          )
-                        : Container(),
-                    // Other product details below the carousel
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            state.product.name ?? "",
-                            style: const TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 8),
-                          state.product.description != null
-                              ? Text(
-                                  state.product.description!,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w200),
-                                )
-                              : Container(),
-                          const SizedBox(height: 8),
-                          state.product.summary != null
-                              ? Text(
-                                  state.product.summary!,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w200),
-                                )
-                              : Container(),
-                          const SizedBox(height: 16),
-                          Text(
-                            state.product.price.toString() + " RS",
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            state.product.offerText.toString(),
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                        ],
-                      ),
+  children: [
+    Positioned(
+      top: 0, // Set top to 0 to ensure it starts from the top
+      left: 0,
+      right: 0,
+      bottom: 80, // Adjust the bottom value to leave space for the button
+      child: ListView(
+        children: [
+          // Image Carousel covering 60% of the screen
+          state.product.images != null &&
+                  state.product.images!.isNotEmpty
+              ? Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: CarouselSlider(
+                    options: CarouselOptions(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      autoPlayInterval: const Duration(seconds: 3),
                     ),
-                  ],
-                ),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: ElevatedButton(
-                            onPressed: () {
-                              // Add your action here
-                              print('Buy Now');
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.red, // Red color for button
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(
-                                    30.0), // Curved button
-                              ),
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 15.0),
-                            ),
-                            child: const Text(
-                              'Buy Now',
-                              style: TextStyle(
-                                  fontSize: 18.0, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                    items: state.product.images!.map((image) {
+                      return Image.network(image);
+                    }).toList(),
                   ),
                 )
+              : Container(),
+          // Other product details below the carousel
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  state.product.name ?? "",
+                  style: const TextStyle(
+                      fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                state.product.description != null
+                    ? Text(
+                        state.product.description!,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w200),
+                      )
+                    : Container(),
+                const SizedBox(height: 8),
+                state.product.summary != null
+                    ? Text(
+                        state.product.summary!,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w200),
+                      )
+                    : Container(),
+                const SizedBox(height: 16),
+                Text(
+                  state.product.price.toString() + " RS",
+                  style: const TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  state.product.offerText.toString(),
+                  style: const TextStyle(fontSize: 20),
+                ),
               ],
-            );
+            ),
+          ),
+        ],
+      ),
+                
+    ),
+    Positioned(
+      left: 0,
+      right: 0,
+      bottom: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                onPressed: () {
+                  // Add your action here
+                  print('Buy Now');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // Red color for button
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0), // Curved button
+                  ),
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                ),
+                child: const Text(
+                  'Buy Now',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  ],
+);
           } else if (state is AppLoadingState) {
             return const Scaffold(
                 backgroundColor: Colors.white,
